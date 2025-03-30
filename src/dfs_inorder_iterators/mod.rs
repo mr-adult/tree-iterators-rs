9f1d441ca83d8c1632083f68b84db33460dda368
Adam Fortune <adamfortune96@protonmail.com>
2025-03-30 17:43:19 -0500

    Flatten/simplify the logic of the DSF In Order iterator

--- src/dfs_inorder_iterators/mod.rs
+++ src/dfs_inorder_iterators/mod.rs
@@ -3,31 +3,24 @@ pub mod mut_borrow;
 pub mod owned;
 
 macro_rules! dfs_inorder_next {
-    ($get_value_and_left_right: ident) => {
+    ($get_value_and_children_binary: ident) => {
         fn next(&mut self) -> Option<Self::Item> {
             self.moved = true;
             let mut current = None;
             while current.is_none() {
-                current = match current {
-                    Some(c) => Some(c),
-                    None => {
-                        if self.right_stack.len() == self.item_stack.len() {
-                            return self.item_stack.pop();
-                        }
-                        match self.right_stack.pop() {
-                            Some(right) => right,
-                            None => None,
-                        }
-                    }
-                };
+                if self.right_stack.len() == self.item_stack.len() {
+                    return self.item_stack.pop();
+                }
 
-                if self.right_stack.len() == 0 {
+                current = self.right_stack.pop().unwrap_or_default();
+
+                if self.right_stack.is_empty() {
                     break;
                 }
             }
 
             while let Some(current_val) = current {
-                let (value, [left, right]) = current_val.$get_value_and_left_right();
+                let (value, [left, right]) = current_val.$get_value_and_children_binary();
 
                 self.right_stack.push(right);
                 self.item_stack.push(value);
@@ -44,53 +37,41 @@ macro_rules! dfs_inorder_streaming_iterator_impl {
         fn advance(&mut self) {
             let mut current = None;
             while current.is_none() {
-                if self.right_stack.len() == 0 {
+                if self.right_stack.is_empty() {
                     self.item_stack.clear();
                     break;
                 }
 
-                while self.status_stack.len() > 0
-                    && self.status_stack[self.status_stack.len() - 1] == TraversalStatus::WentRight
-                {
+                while let Some(TraversalStatus::WentRight) = self.status_stack.last() {
                     self.item_stack.pop();
                     self.status_stack.pop();
                 }
 
-                if self.status_stack.len() > 0 {
-                    let len = self.status_stack.len();
-                    match self.status_stack.get_mut(len - 1) {
-                        Some(status) => {
-                            if *status != TraversalStatus::ReturnedSelf {
-                                *status = TraversalStatus::ReturnedSelf;
-                                return;
-                            }
-                        }
-                        None => {}
+                if let Some(last_status) = self.status_stack.last_mut() {
+                    if !matches!(last_status, TraversalStatus::ReturnedSelf) {
+                        *last_status = TraversalStatus::ReturnedSelf;
+                        return;
                     }
                 }
 
-                current = match current {
-                    Some(c) => Some(c),
-                    None => {
-                        let len = self.item_stack.len();
-                        if self.status_stack.len() > 0 {
-                            self.status_stack[len - 1] = TraversalStatus::WentRight;
-                        }
-                        match self.right_stack.pop() {
-                            Some(right) => right,
-                            None => {
-                                while self.status_stack.len() > 0
-                                    && self.status_stack[self.status_stack.len() - 1]
-                                        == TraversalStatus::WentRight
-                                {
-                                    self.item_stack.pop();
-                                    self.status_stack.pop();
-                                }
-                                return;
-                            }
-                        }
-                    }
-                };
+                if current.is_some() {
+                    continue;
+                }
+
+                if let Some(last_status) = self.status_stack.last_mut() {
+                    *last_status = TraversalStatus::WentRight;
+                }
+
+                if let Some(top_of_right_stack) = self.right_stack.pop() {
+                    current = top_of_right_stack;
+                    continue;
+                }
+
+                while let Some(TraversalStatus::WentRight) = self.status_stack.last() {
+                    self.item_stack.pop();
+                    self.status_stack.pop();
+                }
+                return;
             }
 
             while let Some(current_val) = current {
@@ -102,9 +83,8 @@ macro_rules! dfs_inorder_streaming_iterator_impl {
                 current = left;
             }
 
-            if self.status_stack.len() > 0 {
-                let len = self.status_stack.len();
-                self.status_stack[len - 1] = TraversalStatus::ReturnedSelf;
+            if let Some(last_status) = self.status_stack.last_mut() {
+                *last_status = TraversalStatus::ReturnedSelf;
             }
         }
 
@@ -134,7 +114,6 @@ macro_rules! get_mut {
 /// should be treated as a state machine that can only flow
 /// in one direction
 /// WentLeft -> ReturnedSelf -> WentRight.
-#[derive(PartialEq, Eq)]
 pub(crate) enum TraversalStatus {
     WentLeft,
     ReturnedSelf,
