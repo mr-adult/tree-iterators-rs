f01bb17cb5a35298dfdc9da74891b22345835af0
Adam Fortune <adamfortune96@protonmail.com>
2025-08-21 21:02:19 -0500

    simplify depth-first leaves .next() implementation

--- tree_iterators_rs/src/leaves_iterators/depth_first/mod.rs
+++ tree_iterators_rs/src/leaves_iterators/depth_first/mod.rs
@@ -7,62 +7,39 @@ macro_rules! dfs_postorder_leaves_next {
         fn next(&mut self) -> Option<Self::Item> {
             // keep track of if the children iterator was just added.
             // In some cases this becomes important.
-            let mut just_added = false;
+            let mut just_added = if let Some(root) = self.root.take() {
+                let (value, children) = root.$get_value_and_children();
+                self.traversal_stack_top.push(children.into_iter());
+                self.item_stack.push(value);
+                true
+            } else {
+                false
+            };
+
             loop {
-                match core::mem::take(&mut self.root) {
-                    Some(next) => {
-                        let (value, children) = next.$get_value_and_children();
-                        self.traversal_stack_top.push(children.into_iter());
+                if let Some(next_iter) = self.traversal_stack_top.last_mut() {
+                    if let Some(node) = next_iter.next() {
+                        let (value, children) = node.$get_value_and_children();
                         self.item_stack.push(value);
+                        self.traversal_stack_top.push(children.into_iter());
                         just_added = true;
-                    }
-                    None => {
-                        loop {
-                            let total_stack_len =
-                                self.traversal_stack_bottom.len() + self.traversal_stack_top.len();
-                            if total_stack_len < 1 {
-                                return None;
-                            }
-                            let top_stack_len = self.traversal_stack_top.len();
-                            if top_stack_len > 0 {
-                                match self.traversal_stack_top.get_mut(top_stack_len - 1) {
-                                    None => {
-                                        self.item_stack.pop();
-                                    }
-                                    Some(next_iter) => match next_iter.next() {
-                                        None => {
-                                            self.traversal_stack_top.pop();
-                                            let popped = self.item_stack.pop();
-                                            if just_added {
-                                                return popped;
-                                            }
-                                        }
-                                        Some(node) => {
-                                            let (value, children) = node.$get_value_and_children();
-                                            self.item_stack.push(value);
-                                            self.traversal_stack_top.push(children.into_iter());
-                                            just_added = true;
-                                        }
-                                    },
-                                }
-                                continue;
-                            }
-                            let bottom_stack_len = self.traversal_stack_bottom.len();
-                            match self.traversal_stack_bottom.get_mut(bottom_stack_len - 1) {
-                                None => {}
-                                Some(next_iter) => match next_iter.next() {
-                                    None => {
-                                        self.traversal_stack_bottom.pop();
-                                    }
-                                    Some(node) => {
-                                        let (value, children) = node.$get_value_and_children();
-                                        self.item_stack.push(value);
-                                        self.traversal_stack_top.push(children.into_iter())
-                                    }
-                                },
-                            }
+                    } else {
+                        self.traversal_stack_top.pop();
+                        let popped = self.item_stack.pop();
+                        if just_added {
+                            return popped;
                         }
                     }
+                } else if let Some(next_iter) = self.traversal_stack_bottom.last_mut() {
+                    if let Some(node) = next_iter.next() {
+                        let (value, children) = node.$get_value_and_children();
+                        self.item_stack.push(value);
+                        self.traversal_stack_top.push(children.into_iter())
+                    } else {
+                        self.traversal_stack_bottom.pop();
+                    }
+                } else {
+                    return None;
                 }
             }
         }
